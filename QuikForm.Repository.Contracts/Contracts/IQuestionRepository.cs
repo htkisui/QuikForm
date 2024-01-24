35af15554ee0defa3eb5181c2e388c0aa335bfbe
Elsa <elsa.thievet@hotmail.fr>
2024-01-24 21:27:51 +0100

    Update Question entity with attributes Create IQuestionRepository + QuestionRepository + QuestionRepositoryTest All CRUD actions added to these, except for QuestionRepository.UpdateAsync and QuestionRepositoryTest.UpdateAsync_QuestionToUpdate_QuestionUpdated

---
+++ QuikForm.Repository.Contracts/Contracts/IQuestionRepository.cs
@@ -0,0 +1,42 @@
+﻿using QuikForm.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikForm.Repository.Contracts.Contracts;
+public interface IQuestionRepository
+{
+    /// <summary>
+    /// Create a new question.
+    /// </summary>
+    /// <param name="question"></param>
+    Task CreateAsync(Question question);
+
+    /// <summary>
+    /// Delete a question and its fields.
+    /// </summary>
+    /// <param name="id"></param>
+    Task DeleteAsync(int id);
+
+    /// <summary>
+    /// Get all questions of a form with their fields.
+    /// </summary>
+    /// <returns>List of questions</returns>
+    Task<List<Question>> GetAllAsync();
+
+    /// <summary>
+    /// Get a question by Id with its fields.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Question</returns>
+    Task<Question> GetByIdAsync(int id);
+
+    /// <summary>
+    /// Update a question
+    /// </summary>
+    /// <param name="question"></param>
+    /// <returns>Question updated</returns>
+    Task<Question> UpdateAsync(Question question);
+}
---
+++ QuikForm.Repository.Contracts/Exceptions/Questions/QuestionNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikForm.Repository.Contracts.Exceptions.Questions;
+public class QuestionNotFoundException : Exception
+{
+    public override string Message => "Question not found";
+}
