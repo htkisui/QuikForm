09b0e33fd0bb1f7642c9f56ba62093634822c32c
htkisui <htk.isui.pro@gmail.com>
2024-01-24 12:10:29 +0100

    Add Entities (Answer & CompletedForm)

---
+++ QuikForm.Entities/CompletedForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikForm.Entities;
+public class CompletedForm
+{
+    public int Id { get; set; }
+
+    public List<Answer> Answers { get; set; } = [];
+
+    public int? UserId { get; set; }
+    public User? User { get; set; }
+}
