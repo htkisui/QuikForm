65f6bbddfc40ade830fc6c19fedfe5d567874893
htkisui <htk.isui.pro@gmail.com>
2024-01-25 23:02:19 +0100

    Set front workspace

---
+++ WebApp/Controllers/ElsaController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+public class ElsaController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ WebApp/Controllers/SarahController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+public class SarahController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
