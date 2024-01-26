b4da5443f37f21cdc73cab9452db50165d22294d
Elsa <elsa.thievet@hotmail.fr>
2024-01-26 14:45:23 +0100

    Create ViewComponents for FieldFull, FieldList ; create ViewModels for Field + InputType

---
+++ QuikForm.WebApp/Views/Shared/Components/FieldFull/FieldFullViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuikForm.WebApp.Models.Fields;
+using QuikForm.WebApp.Models.InputTypes;
+
+namespace QuikForm.WebApp.Views.Shared.Components.FieldFull;
+
+public class FieldFullViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(FieldViewModel fieldViewModel, InputTypeViewModel inputTypeViewModel = null)
+    {
+        ViewBag.InputType = inputTypeViewModel?.Name;
+        ViewBag.MockInputType = "checkbox"; //temp (change in cshtml too)
+        return View(fieldViewModel);
+    }
+}
---
+++ QuikForm.WebApp/Views/Shared/Components/FieldList/FieldListViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuikForm.WebApp.Models.Fields;
+using QuikForm.WebApp.Models.InputTypes;
+
+namespace QuikForm.WebApp.Views.Shared.Components.FieldList;
+
+public class FieldListViewComponent: ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(List<FieldViewModel> fieldViewModels)
+    {
+        return View(fieldViewModels);
+    }
+}
