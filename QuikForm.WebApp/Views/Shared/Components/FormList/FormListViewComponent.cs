abecdecedc17c8d452cad088a91ebee98759f9f3
Ellyra34 <leon.sarah24@gmail.com>
2024-01-27 20:09:22 +0100

    FormShortViewComponent Implemented

---
+++ QuikForm.WebApp/Views/Shared/Components/FormList/FormListViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuikForm.WebApp.Models.Forms;
+
+namespace QuikForm.WebApp.Views.Shared.Components.FormList;
+
+//public class FormListViewComponent : ViewComponent
+//{
+//    public async Task<IViewComponentResult> InvocAsync(List<FormViewModel> forms)
+//    {
+//        return View(forms);
+//    }
+//}
--- QuikForm.WebApp/Views/Shared/Components/FormShort/FormShortViewComponent.cs
+++ QuikForm.WebApp/Views/Shared/Components/FormShort/FormShortViewComponent.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuikForm.WebApp.Models.Forms;
+using System.ComponentModel;
 
 namespace QuikForm.WebApp.Views.Shared.Components.FormShort;
 
 public class FormShortViewComponent : ViewComponent
 {
+    public async Task<IViewComponentResult> InvokeAsync(FormViewModel formViewModel)
+    {
+        return View(formViewModel);
+    }
 }
