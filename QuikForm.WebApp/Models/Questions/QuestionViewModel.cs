526f2f35f26715797643b7681b0ca0b9f921a960
Ellyra34 <leon.sarah24@gmail.com>
2024-01-29 17:14:24 +0100

    QuestionsFormList and QuestionForm ViewComponent Init

---
+++ QuikForm.WebApp/Models/Questions/QuestionViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using QuikForm.WebApp.Models.Fields;
+using QuikForm.WebApp.Models.Forms;
+using QuikForm.WebApp.Models.InputTypes;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuikForm.WebApp.Models.Questions;
+
+public class QuestionViewModel
+{
+    public int Id { get; set; }
+
+    public string? Label { get; set; }
+
+    public bool IsMandatory { get; set; }
+
+    //public List<FieldViewModel> FieldViewModels { get; set; } = [];
+
+    //public InputTypeViewModel InputTypeViewModel { get; set; } = null!;
+}
