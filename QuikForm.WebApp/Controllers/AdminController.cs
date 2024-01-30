a7929239a5b4beb3c7d8e3239ef3310b3513c453
htkisui <htk.isui.pro@gmail.com>
2024-01-30 19:30:04 +0100

    Fix ajax create field

---
+++ QuikForm.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuikForm.Business.Contracts.Business;
+
+namespace QuikForm.WebApp.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly IFormBusiness _formBusiness;
+
+    public AdminController(IFormBusiness formBusiness)
+    {
+        _formBusiness = formBusiness;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+
+        return View();
+    }
+}
