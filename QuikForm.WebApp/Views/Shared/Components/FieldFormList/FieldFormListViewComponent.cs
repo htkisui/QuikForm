2a790165e71c12c01e814aee3258eb1625cfb267
Ellyra34 <leon.sarah24@gmail.com>
2024-01-30 16:21:37 +0100

    field.create ajax is fixed

--- QuikForm.WebApp/Views/Shared/Components/FieldFormList/FieldFormListViewComponent.cs
+++ QuikForm.WebApp/Views/Shared/Components/FieldFormList/FieldFormListViewComponent.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuikForm.WebApp.Models.Fields;
+using QuikForm.WebApp.Models.Questions;
 
 namespace QuikForm.WebApp.Views.Shared.Components.FieldFormList;
 
 public class FieldFormListViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync(List<FieldViewModel> fieldViewModels)
+    public async Task<IViewComponentResult> InvokeAsync(List<FieldViewModel> fieldViewModels, int questionId)
     {
+        ViewBag.QuestionId = questionId;
         return View(fieldViewModels);
     }
 }
