46976a08d872e8234bed6c63fc1115e49dd5642c
Elsa <elsa.thievet@hotmail.fr>
2024-02-02 17:42:23 +0100

    create formfull VC and all it's children VCs

--- QuikForm.WebApp/Views/Shared/Components/FieldFullList/FieldFullListViewComponent.cs
+++ QuikForm.WebApp/Views/Shared/Components/FieldFullList/FieldFullListViewComponent.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuikForm.WebApp.Models.Fields;
 using QuikForm.WebApp.Models.InputTypes;
+using QuikForm.WebApp.Models.Questions;
 
 namespace QuikForm.WebApp.Views.Shared.Components.FieldList;
 
 public class FieldFullListViewComponent: ViewComponent
 {
-    public IViewComponentResult Invoke(List<FieldViewModel> fieldViewModels)
+    public IViewComponentResult Invoke(List<FieldViewModel> fieldViewModels, QuestionViewModel questionViewModel)
     {
+        ViewBag.QuestionViewModel = questionViewModel;
         return View(fieldViewModels);
     }
 }
