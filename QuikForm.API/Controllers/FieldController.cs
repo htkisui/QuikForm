6eb608842b634c00ae6af49e7fa022ef6270b3a6
htkisui <htk.isui.pro@gmail.com>
2024-02-05 15:10:33 +0100

    Create API

---
+++ QuikForm.API/Controllers/FieldController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuikForm.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class FieldController : ControllerBase
+{
+}
---
+++ QuikForm.API/Program.cs
@@ -0,0 +1,31 @@
+using QuikForm.Business.Business;
+using QuikForm.Business.Contracts.Business;
+using QuikForm.Repositories.Repositories;
+using QuikForm.Repository.Contracts.Contracts;
+using BusinessMappers = QuikForm.Business.Contracts.Mappers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
